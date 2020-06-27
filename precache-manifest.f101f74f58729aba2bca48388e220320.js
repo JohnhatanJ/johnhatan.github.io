343c7c4e8aa6dc09c6817481748d3c403a9b0abe
JohnhatanJ <48990742+JohnhatanJ@users.noreply.github.com>
2020-06-26 23:51:15 -0300

    Updates

---
+++ precache-manifest.f101f74f58729aba2bca48388e220320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65e0332396741583cb1c2f85caba000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc1cc4bdeee382da4dab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "152a827d06a80c14bea8",
+    "url": "/static/js/2.0d1e01f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d1e01f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1cc4bdeee382da4dab",
+    "url": "/static/js/main.0adc5b5b.chunk.js"
+  },
+  {
+    "revision": "f2f638069cbca6cd9e22",
+    "url": "/static/js/runtime-main.345b86e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
